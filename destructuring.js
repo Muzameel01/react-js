084187c97d5fe0290b51d8fa1a876c7930adbd1f
Muzameel01 <mkmujameel@gmail.com>
2023-02-21 09:58:45 +0530

    day 5 practice

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+const PI= Math.PI
+const E= Math.E
+const SQRT2 = Math.SQRT2
+
+console.log(PI);
+console.log(E);
+console.log(SQRT2);
+
+const number=[2,5,6,7];
+const [a,b,c,d]=number;
+console.log(c);
+
+
---
+++ object.js
@@ -0,0 +1,16 @@
+const mystery="answer"
+const institute="azhar academy"
+const a=7
+const obj={
+    a,
+    b:4,
+    foo(){},
+    xyz:()=>{},
+    ["first name"]:"azhar",
+    [mystery]: 65,
+    institute,
+};
+console.log(obj.answer);
+console.log(obj.a);
+console.log(obj["first name"]);
+console.log(obj.institute);
